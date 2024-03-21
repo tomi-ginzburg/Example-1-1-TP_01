187785550db2d517bba313492b2b7d1a5e55467c
tomi-ginzburg <tginzburg@fi.uba.ar>
2024-03-21 23:59:05 +0000

    1er Commit

--- main.cpp
+++ main.cpp
@@ -3,21 +3,23 @@
 
 int main()
 {
-    DigitalIn gasDetector(D2);
+    DigitalIn gasDetector(D2); // Configuro pin D2 como entrada
 
-    DigitalOut alarmLed(LED1);
+    DigitalOut alarmLed(LED1); // Configuro Led 1 como salida
 
-    gasDetector.mode(PullDown);
+    gasDetector.mode(PullDown); // Configuro el detector con pull down
 
-    alarmLed = OFF;
+    alarmLed = OFF; // Inicio con el led apagado
+
+    printf("%s\n","Hello World");
 
     while (true) {
         if ( gasDetector == ON ) {
-            alarmLed = ON;
+            alarmLed = ON; // Si el detector esta en alto enciendo el led
         }
         
         if ( gasDetector == OFF ) {
-            alarmLed = OFF;
+            alarmLed = OFF; // Si el detector esta en bajo apago el led
         }
     }
 }
